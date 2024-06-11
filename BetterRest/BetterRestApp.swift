546c22183e6d07014cb1896c7963b778742bb7b9
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-06-11 16:27:13 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Manik Lakhanpal on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
